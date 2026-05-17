501d264f6f518368120f971e19a0802b373172be
bhavyasrilakshmiande <bhavyasrilakshmiande@gmail.com>
2026-05-17 15:46:35 +0530

    Add functions to display subject content and check answers

--- script.js
+++ script.js
@@ -1,3 +1,45 @@
+const content =
+document.getElementById("subject-content");
+
+function showBiology(){
+
+content.innerHTML =
+`
+<h2>🧬 Biology</h2>
+<p>
+Biology helps us understand living organisms,
+plants, animals, and the human body.
+</p>
+`;
+
+}
+
+function showPhysics(){
+
+content.innerHTML =
+`
+<h2>⚡ Physics</h2>
+<p>
+Physics explains motion, gravity,
+energy, and forces.
+</p>
+`;
+
+}
+
+function showChemistry(){
+
+content.innerHTML =
+`
+<h2>🧪 Chemistry</h2>
+<p>
+Chemistry studies atoms,
+elements, and chemical reactions.
+</p>
+`;
+
+}
+
 async function askAI() {
 
 const question =
@@ -15,7 +57,8 @@ return;
 result.innerHTML =
 "Loading AI response...";
 
-const API_KEY = "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";
+const API_KEY =
+"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";
 
 try {
 
@@ -23,19 +66,25 @@ const response = await fetch(
 "https://api.groq.com/openai/v1/chat/completions",
 {
 method: "POST",
+
 headers: {
 "Authorization": `Bearer ${API_KEY}`,
 "Content-Type": "application/json"
 },
+
 body: JSON.stringify({
+
 model: "llama3-8b-8192",
+
 messages: [
 {
 role: "user",
 content: question
 }
 ]
+
 })
+
 }
 );
 
@@ -54,6 +103,27 @@ console.log(error);
 
 result.innerHTML =
 "AI connection failed.";
+
+}
+
+}
+
+function checkAnswer(answer){
+
+let quiz =
+document.getElementById("quiz-result");
+
+if(answer=="correct"){
+
+quiz.innerHTML =
+"✅ Correct Answer!";
+
+}
+
+else{
+
+quiz.innerHTML =
+"❌ Wrong Answer";
 
 }
 
