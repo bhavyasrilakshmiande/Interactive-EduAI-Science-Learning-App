1890ea284a99c64501bab0add69179907d3a12e9
bhavyasrilakshmiande <bhavyasrilakshmiande@gmail.com>
2026-05-17 14:39:41 +0530

    Implement error handling for API fetch
    Added error handling and logging for API response.

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ result.innerHTML = "Thinking...";
 
 const API_KEY = "AIzaSyCdOaw7Dj0nT7YaV9AgAt5YCAdlGvAkMPw";
 
+try {
+
 const response = await fetch(
 `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`,
 {
@@ -35,7 +37,20 @@ text: question
 
 const data = await response.json();
 
+console.log(data);
+
 result.innerHTML =
 data.candidates[0].content.parts[0].text;
+
+}
+
+catch(error){
+
+result.innerHTML =
+"Error loading AI response.";
+
+console.log(error);
+
+}
 
 }
